a55947b0d046418e3690ce29e6c1e940f135dee8
Constantin Bilz <cbilz@protonmail.com>
2020-05-25 14:08:36 +0200

    Add tests for CReal

---
+++ Test/Data/Number/CReal.hs
@@ -0,0 +1,46 @@
+module Test.Data.Number.CReal (creal_properties) where
+
+import Data.Number.CReal
+import Numeric.IEEE
+
+import Test.Framework (Test, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
+
+implies x y = y || not x
+dblEq x y = x == y || x == succIEEE y || x == predIEEE y
+
+prop_creal_some_algebraic_equalities :: Bool
+prop_creal_some_algebraic_equalities = all (uncurry (==:))
+  ([ (sqrt 8, 2 * sqrt 2)
+   , (1/(sqrt 2 - sqrt 3 + sqrt 5), (sqrt 2)/4 - (sqrt 3)/6 + (sqrt 30)/12)
+   , (1 - 1, 0)
+   , (2^100, sqrt(2^200))
+   ] :: [(CReal, CReal)])
+
+prop_creal_some_inequalities :: Bool
+prop_creal_some_inequalities = all (uncurry (<))
+  ([ (1, 1 + 2^^(-500))
+   , (-1 - pi^^(-110), -1)
+   , (-2^^(-500), 0)
+   ] :: [(CReal, CReal)])
+
+prop_creal_rational_toDouble_equality :: Rational -> Bool
+prop_creal_rational_toDouble_equality x =
+  fromRational x `dblEq` toDouble (fromRational x)
+
+creal_properties :: Test.Framework.Test
+creal_properties =
+  testGroup "CReal Properties" [
+    testProperty
+      "creal/check some algebraic identities"
+      prop_creal_some_algebraic_equalities,
+
+    testProperty
+      "creal/check some difficult inequalities"
+      prop_creal_some_inequalities,
+
+    testProperty
+      "creal/convert Rational to Double via CReal"
+      $ withMaxSuccess 10000 prop_creal_rational_toDouble_equality
+  ]
--- TestSuite.hs
+++ TestSuite.hs
@@ -7,9 +7,10 @@ import Test.Framework (
   )
 
 import Test.Data.Number.BigFloat (bigfloat_properties)
+import Test.Data.Number.CReal (creal_properties)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: [Test.Framework.Test]
-tests = [ bigfloat_properties ]
+tests = [ bigfloat_properties, creal_properties ]
